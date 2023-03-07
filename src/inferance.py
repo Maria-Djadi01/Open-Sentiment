60e86ea3da6095c7826459b5206fb23b6db9fefd
Maria-Djadi01 <maria.djadi01@gmail.com>
2023-03-07 20:37:54 +0100

    Update the dashboard

--- src/inferance.py
+++ src/inferance.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import altair as alt
 from transformers import pipeline
 from transformers import BertTokenizer, BertForSequenceClassification
 
@@ -5,45 +7,53 @@ tokenizer = BertTokenizer.from_pretrained("alger-ia/dziribert_sentiment")
 model = BertForSequenceClassification.from_pretrained("alger-ia/dziribert_sentiment")
 
 
-def text_split(text):
-    return text.split('\n')
-
-
-
-def sent_analy_daridja(text):
+def plot(nb_feedbacks):
     dict = {
-        'Sentiment' : ['positive', 'neutral', 'negative'],
-        'Number of feedbacks' : [0, 0, 0]
+        'Sentiment': ['positive', 'negative', 'neutral'],
+        'Number of feedbacks': nb_feedbacks
+        
     }
-    
+    source = pd.DataFrame(dict)
+    plt = alt.Chart(source).mark_bar().encode(
+        x='Sentiment',
+        y='Number of feedbacks',
+    ).properties(
+            width=550,
+            height=200
+    )
+
+    return plt
+
+def sent_analy_daridja_text(text):
     sentiment_model = pipeline("text-classification", model="alger-ia/dziribert_sentiment")
-    result = sentiment_model(text_split(text))
-    
-    for i in result:
-        if i['label'] == 'positive':
-            dict['Number of feedbacks'][0] += 1
-        elif i['label'] == 'neutral':
-            dict['Number of feedbacks'][1] += 1
-        else:
-            dict['Number of feedbacks'][2] += 1
-    return dict
+    result = sentiment_model(text)
+    return result[0]['label']
 
-def sent_analy_not_daridja(text):
-    dict = {
-        'Sentiment' : ['positive', 'neutral', 'negative'],
-        'Number of feedbacks' : [0, 0, 0]
-    }
-    
+
+def sent_analy_not_daridja_text(text):
+
     classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
     candidate_labels = ['positive', 'neutral', 'negative']
     result = classifier(text, candidate_labels)
+    return result;
 
-    for i in result:
-        if i['label'][0] == 'positive':
-            dict['Number of feedbacks'][0] += 1
-        elif i['label'][0] == 'neutral':
-            dict['Number of feedbacks'][1] += 1
-        else:
-            dict['Number of feedbacks'][2] += 1
-    return dict
 
+def sent_analy_daridja_file(df):
+    sentiment_model = pipeline("text-classification", model="alger-ia/dziribert_sentiment")
+    
+    df['Sentiment'] = df['Text'].apply(lambda text: sentiment_model(text)[0]['label'])
+    print(df)
+    
+    count = df['Sentiment'].value_counts().to_dict()
+    print(count)
+    return plot([count['positive'], count['neutral'], count['negative']])
+
+def sent_analy_not_daridja_file(df):
+    classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
+    candidate_labels = ['positive', 'neutral', 'negative']
+
+    df['Sentiment'] = df['Text'].apply(lambda text: classifier(text, candidate_labels)['labels'][0])
+    
+    count = df['Sentiment'].value_counts().to_dict()
+
+    return (df.to_csv('out.csv') , plot([count['positive'], count['neutral'], count['negative']]))
