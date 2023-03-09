28324cf4c7eeaaf0030168582bc13039f933f446
Maria-Djadi01 <maria.djadi01@gmail.com>
2023-03-09 14:07:33 +0100

    Add the classification model to dashboard

--- src/inferance.py
+++ src/inferance.py
@@ -1,15 +1,20 @@
 import pandas as pd
 import altair as alt
 from transformers import pipeline
-from transformers import BertTokenizer, BertForSequenceClassification
+from transformers import BertTokenizer, BertForSequenceClassification, AutoTokenizer, AutoModelForSequenceClassification
 
 tokenizer = BertTokenizer.from_pretrained("alger-ia/dziribert_sentiment")
 model = BertForSequenceClassification.from_pretrained("alger-ia/dziribert_sentiment")
 
+model_path = "../models/classification_model"
+
+model = AutoModelForSequenceClassification.from_pretrained(model_path)
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+
 
 def plot(nb_feedbacks):
     dict = {
-        'Sentiment': ['positive', 'negative', 'neutral'],
+        'Sentiment': ['positive', 'neutral', 'negative'],
         'Number of feedbacks': nb_feedbacks
         
     }
@@ -42,18 +47,60 @@ def sent_analy_daridja_file(df):
     sentiment_model = pipeline("text-classification", model="alger-ia/dziribert_sentiment")
     
     df['Sentiment'] = df['Text'].apply(lambda text: sentiment_model(text)[0]['label'])
-    print(df)
     
     count = df['Sentiment'].value_counts().to_dict()
-    print(count)
-    return plot([count['positive'], count['neutral'], count['negative']])
+    out_file_name = 'open_sentiment.csv'
+    df.to_csv(out_file_name, index=False)
+    return out_file_name, plot([count['positive'], count['neutral'], count['negative']])
 
 def sent_analy_not_daridja_file(df):
     classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
     candidate_labels = ['positive', 'neutral', 'negative']
 
     df['Sentiment'] = df['Text'].apply(lambda text: classifier(text, candidate_labels)['labels'][0])
-    
+
     count = df['Sentiment'].value_counts().to_dict()
 
-    return (df.to_csv('out.csv') , plot([count['positive'], count['neutral'], count['negative']]))
+    out_file_name = 'open_sentiment.csv'
+    df.to_csv(out_file_name)
+    return out_file_name, plot([count['positive'], count['neutral'], count['negative']])
+
+def sent_analy_mixed_language_text(text):
+    return sent_analy_daridja_text(text) if classify_text(text) == 1 else sent_analy_not_daridja_text(text)
+
+def sent_analy_mixed_language_file(df):
+    # Daridja classifier
+    sentiment_model = pipeline("text-classification", model="alger-ia/dziribert_sentiment")
+    
+    # not daridja classifier
+    classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
+    candidate_labels = ['positive', 'neutral', 'negative']
+
+    df["classification"] = df["Text"].apply(classify_text)
+    daridja_df = df[df['classification'] == 1]
+    not_daridja_df = df[df['classification'] == 0]
+
+    daridja_df['Sentiment'] = daridja_df['Text'].apply(lambda text: sentiment_model(text)[0]['label'])
+
+    not_daridja_df['Sentiment'] = not_daridja_df['Text'].apply(lambda text: classifier(text, candidate_labels)['labels'][0])
+    
+    daridja_df.drop(columns=['classification'], inplace=True)
+    not_daridja_df.drop(columns=['classification'], inplace=True)
+    
+    df_final = pd.concat([not_daridja_df, not_daridja_df])
+
+    count = df['Sentiment'].value_counts().to_dict()
+
+    out_file_name = 'open_sentiment.csv'
+    df_final.to_csv(out_file_name)
+    return out_file_name, plot([count['positive'], count['neutral'], count['negative']])
+
+
+
+
+def classify_text(text):
+    inputs = tokenizer(text, return_tensors="pt")
+    outputs = model(**inputs)
+    probs = outputs.logits.softmax(dim=1)
+    pred = probs.argmax().item()
+    return pred
--- ui/dashboard.py
+++ ui/dashboard.py
@@ -1,52 +1,70 @@
 import gradio as gr
 import altair as alt
 import pandas as pd
-from inferance import sent_analy_daridja, sent_analy_not_daridja
+from inferance import sent_analy_daridja_text, sent_analy_not_daridja_text, sent_analy_daridja_file, sent_analy_not_daridja_file, sent_analy_mixed_language_file, sent_analy_mixed_language_text
 
 
 def plot_dict(dict):
     source = pd.DataFrame(dict)
 
-    plt = alt.Chart(source).mark_bar().encode(
+    chart = alt.Chart(source).mark_bar(color="darkblue").encode(
         x='Sentiment',
         y='Number of feedbacks',
     ).properties(
-            width=550,
-            height=200
+        title='Open Sentiment analysis',
     )
 
-    return plt
+    return chart
 
-def sentiment(text, daridja):
-    result = sent_analy_daridja(text) if daridja else sent_analy_not_daridja(text)
-    return(plot_dict(result))
+def sentiment(text_input, dar, ar, fr, en):
+    if(not dar and (ar or fr or en)):
+        return sent_analy_not_daridja_text(text_input)
+    elif(dar and not(ar or fr or en)):
+        return sent_analy_daridja_text(text_input)
+    else: return sent_analy_mixed_language_text(text_input)
+
+def sentiment_file(file_input, dar, ar, fr, en):
+    
+    df = pd.read_csv(file_input.name, names=['Text'])
+    print(df.columns)
+    print(df.Text)
+    if(not dar and (ar or fr or en)):
+        return sent_analy_not_daridja_file(df)
+    elif(dar and not(ar or fr or en)):
+        return sent_analy_daridja_file(text_input)
+    else: sent_analy_mixed_language_file(df)
+
+
+
 
 with gr.Blocks() as demo:
-    gr.Markdown("## Sentiment Analysis")
+    gr.Markdown("# Sentiment Analysis")
     
     with gr.Tab("Mixed language sentiment classifier"):
         with gr.Column():
-            gr.Markdown("### Select the languages you wanna analyze with")
+            gr.Markdown("## Select the languages")
             dar = gr.Checkbox(label="Daridja")
+            ar = gr.Checkbox(label="Arabic")
             fr = gr.Checkbox(label="French")
             en = gr.Checkbox(label="English")
         with gr.Row():
-            file_input = gr.inputs.File(label="Upload a file")
-            file_output = gr.outputs.File(label="Download the file")  
+            file_input = gr.File(label="Upload a file")
+            file_output = gr.File(label="Download CSV", interactive=False)  
         analyze_button1 = gr.Button(label="Analyze")
-        analyze_button1.click(fn=sentiment, inputs=file_input, outputs=file_output)
         result_ml = gr.Plot(label="Sentiment analysis")
+        analyze_button1.click(fn=sentiment_file, inputs=[file_input, dar, ar, fr, en], outputs=[file_output, result_ml])
     
     with gr.Tab("Specific input classifier"):
         with gr.Column():
-            gr.Markdown("### Select the languages you wanna analyze with")
+            gr.Markdown("## Select the languages")
             dar = gr.Checkbox(label="Daridja")
+            ar = gr.Checkbox(label="Arabic")
             fr = gr.Checkbox(label="French")
             en = gr.Checkbox(label="English")
         with gr.Row():
             text_input = gr.Textbox(label="Enter a text")
             output = gr.Textbox(label="Result")
         analyze_button2 = gr.Button(label="Analyze")
-        analyze_button2.click(fn = sentiment, inputs=text_input, outputs=output)
-        
+        analyze_button2.click(fn = sentiment, inputs=[text_input, dar, ar, fr, en], outputs=output)
+
 demo.launch()
